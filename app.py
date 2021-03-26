f8a57293895a6ee58ac7e46e7b7f930f4689c81c
Resat Serpek <rserpek4@hotmail.com>
2021-03-26 22:18:55 +0000

    running...

--- app.py
+++ app.py
@@ -48,10 +48,106 @@ def trading():
 
     for row in g.query(query):
         name, value = row['name'], row['value']
-        data.append({'label': name, 'y': value})
         print(name, value)
+        data.append({'label': name, 'y': value})
     
     return{'data': data}
+@app.route('/api/applied')
+def applied():
+    scheme = request.args.get('status')
+    filter = request.args.get('filter')
+    
+    
+    query = """
+    PREFIX ex: <http://example.org/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?name ?value
+    WHERE {
+        ?type a <http://example.org/"""+ filter.replace(' Size','') + """/>.
+        ?app rdf:value 'Applied'.
+        ?type dcterms:title ?name.
+        ?type ?app ?scheme.
+        ?scheme foaf:name '"""+ scheme + """'.
+        ?scheme rdf:value ?value
+    }
+       
+    """
+    data = []
+
+    for row in g.query(query):
+        name, value = row['name'], row['value']
+        print(name, value)
+        data.append({'label': name, 'y': value})
+    
+    return{'data': data}
+
+@app.route('/api/received')
+def received():
+    scheme = request.args.get('status')
+    filter = request.args.get('filter')
+    
+    
+    query = """
+    PREFIX ex: <http://example.org/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?name ?value
+    WHERE {
+        ?type a <http://example.org/"""+ filter + """/>.
+        ?app rdf:value 'Received'.
+        ?type dcterms:title ?name.
+        ?type ?app ?scheme.
+        ?scheme foaf:name '"""+ scheme + """'.
+        ?scheme rdf:value ?value
+    }
+       
+    """
+    data = []
+
+    for row in g.query(query):
+        name, value = row['name'], row['value']
+        print(name, value)
+        data.append({'label': name, 'y': value})
+    
+    return{'data': data}
+
+@app.route('/api/intending')
+def intending():
+    scheme = request.args.get('status')
+    filter = request.args.get('filter')
+    
+    
+    query = """
+    PREFIX ex: <http://example.org/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?name ?value
+    WHERE {
+        ?type a <http://example.org/"""+ filter + """/>.
+        ?app rdf:value 'Intending'.
+        ?type dcterms:title ?name.
+        ?type ?app ?scheme.
+        ?scheme foaf:name '"""+ scheme + """'.
+        ?scheme rdf:value ?value
+    }
+       
+    """
+    data = []
+
+    for row in g.query(query):
+        name, value = row['name'], row['value']
+        print(name, value)
+        data.append({'label': name, 'y': value})
+    
+    return{'data': data}
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)   
